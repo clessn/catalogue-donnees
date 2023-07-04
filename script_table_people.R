ace5736729b950ed44de38f993b228b623d40bdd
JerGil <gilbertjeremy.2000@gmail.com>
2023-07-04 14:38:33 -0400

    Added vectors to people table)

--- script_table_people.R
+++ script_table_people.R
@@ -16,3 +16,24 @@ Journalists1 <- Journalists1 %>%
   mutate(data.fullName = paste(data.firstName, data.lastName))
 
 Data <- bind_rows(Journalists1, Journalists2)
+
+Data$id1 <- NA
+Data$id1 <- tolower(Data$data.fullName)
+Data$id2 <- gsub(" ", "", Data$id1)
+
+
+
+CleanData <- Data %>% 
+  select(full_name = data.fullName, female = data.isFemale, twitter_handle = data.twitterHandle, 
+         current_media = data.currentMedia, country = metadata.country,
+         province_or_state = metadata.province_or_state)
+  
+  
+CleanData$country <-  sub("CA", "CAN", CleanData$country)
+
+
+
+CleanData$id <- NA
+CleanData$id <- paste0(Data$id2, "1")
+
+
