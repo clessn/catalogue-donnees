672749e6dcfe046fc53c26964474e1d1ccecad8c
jdrouin13 <jeremie.drouin13@gmail.com>
2023-08-31 13:27:00 -0400

    ajout du fichier de merging complet

--- SES_merging.R
+++ SES_merging.R
@@ -114,4 +114,176 @@ imput_from_source("Quorum", "ses_educ_coll", "ses_educDEC")
 imput_from_source("Quorum", "ses_educ_univBA", "ses_educUniv")
 imput_from_source("Quorum", "ses_educ_univOverBA", "ses_educUniv") # Arranger ça
 
+## Religion ##
+
+
+#cleaning de la variable religion dans omnibus
+
+Omnibus$ses_religion_jewish <- NA
+Omnibus$ses_religion_jewish[Omnibus$ses_religion == "Juda\xefsme"] <- 1
+Omnibus$ses_religion_jewish[Omnibus$ses_religion != "Juda\xefsme"] <- 0
+table(Omnibus$ses_religion_jewish)
+
+Omnibus$ses_religion_muslim <- NA
+Omnibus$ses_religion_muslim[Omnibus$ses_religion == "Islam"] <- 1
+Omnibus$ses_religion_muslim[Omnibus$ses_religion != "Islam"] <- 0
+table(Omnibus$ses_religion_muslim)
+
+Omnibus$ses_religion_protest <- NA
+Omnibus$ses_religion_protest[Omnibus$ses_religion == "Protestantisme"] <- 1
+Omnibus$ses_religion_protest[Omnibus$ses_religion != "Protestantisme"] <- 0
+table(Omnibus$ses_religion_protest)
+
+Omnibus$ses_religion_none <- NA
+
+Omnibus$ses_religion_none[Omnibus$ses_religion =="Ath\xe9isme/Aucune" | 
+                            Omnibus$ses_religion == "Agnosticisme"] <- 1
+Omnibus$ses_religion_none[Omnibus$ses_religion !="Ath\xe9isme/Aucune" & 
+                            Omnibus$ses_religion != "Agnosticisme"] <- 0
+table(Omnibus$ses_religion_none)
+
+Omnibus$ses_religion_other <- NA
+Omnibus$ses_religion_other[Omnibus$ses_religion == "Autre" |
+                             Omnibus$ses_religion == "Bouddhisme" |
+                             Omnibus$ses_religion == "Hindouisme" |
+                             Omnibus$ses_religion == "Sikhisme"] <- 1
+Omnibus$ses_religion_other[!(Omnibus$ses_religion %in% c("Autre", "Bouddhisme", 
+                                                         "Hindouisme", "Sikhisme"))] <- 0
+table(Omnibus$ses_religion_other)
+
+Omnibus$ses_religion_christianity <- NA
+Omnibus$ses_religion_christianity[Omnibus$ses_religion == "Catholicisme" |
+                                    Omnibus$ses_religion == "Protestantisme" |
+                                    Omnibus$ses_religion == "Orthodoxe"] <- 1
+Omnibus$ses_religion_christianity[!(Omnibus$ses_religion %in% c("Catholicisme", "Protestantisme", 
+                                                                "Orthodoxe"))] <- 0
+table(Omnibus$ses_religion_christianity)
+
+
+imput_from_source("GlobalES", "relProtest", "ses_religion_protest")
+imput_from_source("GlobalES", "relCatho", "ses_religion_catho")
+imput_from_source("GlobalES", "relJewish", "ses_religion_jewish")
+imput_from_source("GlobalES", "relMuslim", "ses_religion_muslim")
+imput_from_source("GlobalES", "relNone", "ses_religion_none")
+imput_from_source("GlobalES", "relAllOther", "ses_religion_other")
+imput_from_source("GlobalES", "relAllChrist", "ses_religion_christianity")
+imput_from_source("INSPQ", "relProtest", "ses_religion_protest")
+imput_from_source("INSPQ", "relCatho", "ses_religion_catho")
+imput_from_source("INSPQ", "relJewish", "ses_religion_jewish")
+imput_from_source("INSPQ", "relMuslim", "ses_religion_muslim")
+imput_from_source("INSPQ", "relNone", "ses_religion_none")
+imput_from_source("INSPQ", "relAllChrist", "ses_religion_christianity")
+imput_from_source("Omnibus", "ses_isCatho", "ses_religion_catho")
+imput_from_source("Omnibus", "ses_religion_protest", "ses_religion_protest")
+imput_from_source("Omnibus", "ses_religion_muslim", "ses_religion_muslim")
+imput_from_source("Omnibus", "ses_religion_jewish", "ses_religion_jewish")
+imput_from_source("Omnibus", "ses_religion_other", "ses_religion_other")
+imput_from_source("Omnibus", "ses_religion_none", "ses_religion_none")
+imput_from_source("Omnibus", "ses_religion_christianity", "ses_religion_christianity")
+
+#### Income ####
+
+imput_from_source("GlobalES", "incomeLow", "ses_income_low")
+imput_from_source("GlobalES", "incomeMid", "ses_income_mid")
+imput_from_source("GlobalES", "incomeHigh", "ses_income_high")
+imput_from_source("INSPQ", "incomeLow", "ses_income_low")
+imput_from_source("INSPQ", "incomeMid", "ses_income_mid")
+imput_from_source("INSPQ", "incomeHigh", "ses_income_high")
+imput_from_source("Omnibus", "ses_incomeLow", "ses_income_low")
+imput_from_source("Omnibus", "ses_incomeMid", "ses_income_mid")
+imput_from_source("Omnibus", "ses_incomeHigh", "ses_income_high")
+imput_from_source("Pilot2_2021", "incomeLow", "ses_income_low")
+imput_from_source("Pilot2_2021", "incomeMid", "ses_income_mid")
+imput_from_source("Pilot2_2021", "incomeHigh", "ses_income_high")
+imput_from_source("Quorum", "ses_income_low", "ses_income_low")
+imput_from_source("Quorum", "ses_income_mid", "ses_income_mid")
+imput_from_source("Quorum", "ses_income_high", "ses_income_high")
+
+####Sexual orientation####
+
+imput_from_source("Pilot2_2021", "ses_hetero", "ses_orientation_hetero")
+imput_from_source("Pilot2_2021", "ses_gai", "ses_orientation_homosexual")
+imput_from_source("Pilot2_2021", "ses_bisex", "ses_orientation_bisexual")
+imput_from_source("INSPQ", "heterosexual", "ses_orientation_hetero")
+imput_from_source("INSPQ", "homosexual", "ses_orientation_homosexual")
+imput_from_source("INSPQ", "bisexual", "ses_orientation_bisexual")
+
+####Mariage status####
+
+imput_from_source("INSPQ", "married", "ses_married")
+imput_from_source("GlobalES", "married", "ses_married")
+
+#### Province ####
+
+imput_from_source("INSPQ", "nfld", "ses_province_nfld")
+imput_from_source("INSPQ", "pei", "ses_province_pei")
+imput_from_source("INSPQ", "ns", "ses_province_ns")
+imput_from_source("INSPQ", "nb", "ses_province_nb")
+imput_from_source("INSPQ", "quebec", "ses_province_quebec")
+imput_from_source("INSPQ", "ontario", "ses_province_ontario")
+imput_from_source("INSPQ", "manitoba", "ses_province_manitoba")
+imput_from_source("INSPQ", "sask", "ses_province_sask")
+imput_from_source("INSPQ", "alberta", "ses_province_alberta")
+imput_from_source("INSPQ", "bc", "ses_province_bc")
+imput_from_source("INSPQ", "north", "ses_province_north")
+imput_from_source("GlobalES", "nfld", "ses_province_nfld")
+imput_from_source("GlobalES", "pei", "ses_province_pei")
+imput_from_source("GlobalES", "ns", "ses_province_ns")
+imput_from_source("GlobalES", "nb", "ses_province_nb")
+imput_from_source("GlobalES", "quebec", "ses_province_quebec")
+imput_from_source("GlobalES", "ontario", "ses_province_ontario")
+imput_from_source("GlobalES", "manitoba", "ses_province_manitoba")
+imput_from_source("GlobalES", "sask", "ses_province_sask")
+imput_from_source("GlobalES", "alberta", "ses_province_alberta")
+imput_from_source("GlobalES", "bc", "ses_province_bc")
+imput_from_source("GlobalES", "nunavut", "ses_province_nunavut")
+imput_from_source("Pilot2_2021", "ses_prov_Nfl", "ses_province_nfld")
+imput_from_source("Pilot2_2021", "ses_prov_Pei", "ses_province_pei")
+imput_from_source("Pilot2_2021", "ses_prov_Ns", "ses_province_ns")
+imput_from_source("Pilot2_2021", "ses_prov_Nb", "ses_province_nb")
+imput_from_source("Pilot2_2021", "quebec", "ses_province_quebec")
+imput_from_source("Pilot2_2021", "ontario", "ses_province_ontario")
+imput_from_source("Pilot2_2021", "ses_prov_Manitoba", "ses_province_manitoba")
+imput_from_source("Pilot2_2021", "ses_prov_Skt", "ses_province_sask")
+imput_from_source("Pilot2_2021", "ses_prov_Alb", "ses_province_alberta")
+imput_from_source("Pilot2_2021", "ses_prov_Bc", "ses_province_bc")
+imput_from_source("Pilot2_2021", "ses_prov_Yukon", "ses_province_yukon")
+imput_from_source("Quorum", "ses_QC", "ses_province_quebec")
+
+#### Immigration ####
+
+imput_from_source("Pilot2_2021", "immigrant", "ses_immigrant")
+imput_from_source("GlobalES", "immigrant", "ses_immigrant")
+imput_from_source("INSPQ", "immigrant", "ses_immigrant")
+
+#### Urban/Rural ####
+
+imput_from_source("GlobalES", "urban", "ses_urban")
+imput_from_source("GlobalES", "rural", "ses_rural")
+imput_from_source("Pilot2_2021", "ses_urbain", "ses_urban")
+imput_from_source("Pilot2_2021", "ses_rural", "ses_rural")
+imput_from_source("Omnibus", "ses_mtlIsle", "ses_urban")
+imput_from_source("Omnibus", "ses_mtl", "ses_urban")
+imput_from_source("Omnibus", "ses_qcCity", "ses_urban")
+imput_from_source("Omnibus", "ses_noMtlQc", "ses_urban")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
