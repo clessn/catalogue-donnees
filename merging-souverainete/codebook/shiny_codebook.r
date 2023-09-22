66590784a80b844e7aa0f1c8d71d306fe2571a32
Laurence-Olivier M. Foisy <mail@mfoisy.com>
2023-09-22 16:19:37 -0400

    shiny displays everything

--- merging-souverainete/codebook/shiny_codebook.r
+++ merging-souverainete/codebook/shiny_codebook.r
@@ -43,32 +43,31 @@ server <- function(input, output, session) {
   source_list <- c("ces1965", 
                    "ces1968", 
                    "ces1974", 
-                   "ces1979", 
+                   "ces1979",        
                    "ces1984", 
                    "ces1988",
-                   "ces1993",
-                   "ces1997",
-                   "ces2000",
-                   "ces2004",
-                   "ces2006",
-                   "ces2008",
-                   "ces2011",
-                   "ces2015",
-                   "ces2019",
-                   "ces2021",
-                   "datagotchi_pilot1_2021",
-                   "omnibusjanuary",
-                   "omnibusfebruary",
-                   "omnibusmarch",
-                   "omnibusapril",
-                   "omnibusmay",
-                   "omnibusjune",
-                   "datagotchi_pilot2_2022",
-                   "sondage_nationalisme_2022",
-                   "quorum_mcq",
-                   "pes_2022",
-                   "pco_survey"
-                    )
+                   "ces1993", 
+                   "ces1997", 
+                   "ces2000", 
+                   "ces2004", 
+                   "ces2006", 
+                   "ces2008", 
+                   "ces2011", 
+                   "ces2015", 
+                   "ces2019", 
+                   "ces2021", 
+                   "datagotchi_pilot1_2021", 
+                   "omnibusjanuary", 
+                   "omnibusfebruary", 
+                   "omnibusmarch", 
+                   "omnibusapril", 
+                   "omnibusmay", 
+                   "omnibusjune", 
+                   "datagotchi_pilot2_2022", 
+                   "sondage_nationalisme_2022", 
+                   "quorum_mcq", 
+                   "pes_2022", 
+                   "pco_survey")
 
   observe({
     json_files <- list.files(path = getwd(), pattern = "*.json", full.names = FALSE)
@@ -82,7 +81,7 @@ server <- function(input, output, session) {
     codebookData(json_data)
   })
   
-    output$dynamicUI <- renderUI({
+  output$dynamicUI <- renderUI({
     json_data <- codebookData()
     
     # Initialize UI elements list
@@ -93,26 +92,31 @@ server <- function(input, output, session) {
     
     # Loop through each source_id and generate UI elements
     for (id in names(json_data$source_id)) {
-      source_data <- json_data$source_id[[id]]
-      
-      # Add source_id as title, question_label as subtitle, and class as subsubtitle
-      ui_elements[[length(ui_elements) + 1]] <- HTML(paste("<h3>", id, "</h3><h4>", source_data$question_label, "</h4><h5>Class: ", source_data$class, "</h5>"))
-      
-      # Create a table for choices
-      choices_df <- as.data.frame(source_data$choices)
-      table_id <- paste0("table_", gsub("[^a-zA-Z0-9]", "_", id))
-      ui_elements[[length(ui_elements) + 1]] <- tableOutput(table_id)
-      
-      # Render the table
-      output[[table_id]] <- renderTable({
-        choices_df
-      })
+      # Use a local function to capture the current value of id
+      makeUIElement <- function(id) {
+        source_data <- json_data$source_id[[id]]
+        
+        # Add source_id as title, question_label as subtitle, and class as subsubtitle
+        ui_elements[[length(ui_elements) + 1]] <- HTML(paste("<h3>", id, "</h3><h4>", source_data$question_label, "</h4><h5>Class: ", source_data$class, "</h5>"))
+        
+        # Create a table for choices
+        choices_df <- as.data.frame(source_data$choices)
+        table_id <- paste0("table_", gsub("[^a-zA-Z0-9]", "_", id))
+        ui_elements[[length(ui_elements) + 1]] <- tableOutput(table_id)
+        
+        # Render the table
+        output[[table_id]] <- renderTable({
+          choices_df
+        })
+        
+        return(ui_elements)
+      }
+      ui_elements <- makeUIElement(id)
     }
     
     # Return the list of UI elements
     do.call(tagList, ui_elements)
   })
-  
 }
 
 # Run the Application
