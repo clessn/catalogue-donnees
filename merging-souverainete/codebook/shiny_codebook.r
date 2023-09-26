a9fb5d0d86ae159f6f41373210a7459f7a2c9be6
etienneprx <126438196+etienneprx@users.noreply.github.com>
2023-09-26 12:02:17 +0200

    ajout ses_age jusqu'à CES88

---
+++ merging-souverainete/codebook/shiny_codebook.r
@@ -0,0 +1,123 @@
+# Load Required Libraries
+library(shiny)
+library(shinyWidgets)
+library(jsonlite)
+
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+      body::after {
+        content: '';
+        display: block;
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: -1;
+        background-image: url('https://scontent-cdg4-2.xx.fbcdn.net/v/t39.30808-6/383012596_272442245615644_8584141306423411014_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5614bc&_nc_ohc=4Oi2Ic8W4jgAX89Q0SG&_nc_ht=scontent-cdg4-2.xx&oh=00_AfBkgflD0DXSh1jHTOyS13qSa0xJywGqHf60me9YxIXC1w&oe=651813F0');
+        background-size: cover;
+        opacity: 1;
+      }
+      .container-fluid, .shiny-output-error {
+        background-color: rgba(255, 255, 255, 0.9) !important;
+      }
+    "))
+  ),
+  titlePanel("Codebook souveraineté - ON VA L'AVOIR NOTRE PAYS!"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("fileSelect", "Sélectionnez votre variable", "")
+    ),
+    mainPanel(
+      uiOutput("dynamicUI")
+    )
+  )
+)
+
+# Define Server Logic
+server <- function(input, output, session) {
+  
+  codebookData <- reactiveVal(list())
+  
+  source_list <- c("ces1965", 
+                   "ces1968", 
+                   "ces1974", 
+                   "ces1979",        
+                   "ces1984", 
+                   "ces1988",
+                   "ces1993", 
+                   "ces1997", 
+                   "ces2000", 
+                   "ces2004", 
+                   "ces2006", 
+                   "ces2008", 
+                   "ces2011", 
+                   "ces2015", 
+                   "ces2019", 
+                   "ces2021", 
+                   "datagotchi_pilot1_2021", 
+                   "omnibusjanuary", 
+                   "omnibusfebruary", 
+                   "omnibusmarch", 
+                   "omnibusapril", 
+                   "omnibusmay", 
+                   "omnibusjune", 
+                   "datagotchi_pilot2_2022", 
+                   "sondage_nationalisme_2022", 
+                   "quorum_mcq", 
+                   "pes_2022", 
+                   "pco_survey")
+
+  observe({
+    json_files <- list.files(path = getwd(), pattern = "*.json", full.names = FALSE)
+    updateSelectInput(session, "fileSelect", choices = json_files)
+  })
+  
+  observeEvent(input$fileSelect, {
+    req(input$fileSelect)
+    selected_file <- file.path(getwd(), input$fileSelect)
+    json_data <- fromJSON(selected_file)
+    codebookData(json_data)
+  })
+  
+  output$dynamicUI <- renderUI({
+    json_data <- codebookData()
+    
+    # Initialize UI elements list
+    ui_elements <- list()
+    
+    # Add description as title
+    ui_elements[[length(ui_elements) + 1]] <- HTML(paste("<h2>", json_data$description, "</h2>"))
+    
+    # Loop through each source_id and generate UI elements
+    for (id in names(json_data$source_id)) {
+      # Use a local function to capture the current value of id
+      makeUIElement <- function(id) {
+        source_data <- json_data$source_id[[id]]
+        
+        # Add source_id as title, question_label as subtitle, and class as subsubtitle
+        ui_elements[[length(ui_elements) + 1]] <- HTML(paste("<h3>", id, "</h3><h4>", source_data$question_label, "</h4><h5>Class: ", source_data$class, "</h5>"))
+        
+        # Create a table for choices
+        choices_df <- as.data.frame(source_data$choices)
+        table_id <- paste0("table_", gsub("[^a-zA-Z0-9]", "_", id))
+        ui_elements[[length(ui_elements) + 1]] <- tableOutput(table_id)
+        
+        # Render the table
+        output[[table_id]] <- renderTable({
+          choices_df
+        })
+        
+        return(ui_elements)
+      }
+      ui_elements <- makeUIElement(id)
+    }
+    
+    # Return the list of UI elements
+    do.call(tagList, ui_elements)
+  })
+}
+
+# Run the Application
+shinyApp(ui, server)
