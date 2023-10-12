207f20bfcce095a09d368fb34b1da7c42531db31
etienneprx <126438196+etienneprx@users.noreply.github.com>
2023-10-12 17:03:51 +0200

    Salut la ribambelle! J'ai fini ses_lang pour le codebook et j'ai avancé ses_age.1.R (ou ses_age__1.R ????) jusqu'à ces1988

--- merging-souverainete/merging_files/ses_age.1.R
+++ merging-souverainete/merging_files/ses_age.1.R
@@ -13,7 +13,7 @@ names(output_age) <- ids
 # Merging and cleaning ----------------------------------------------------
 
 ## ces65 -------------------------------------------------------------------
-#### 1. Get raw gender variable vector
+#### 1. Get raw age variable vector
 
 raw_ces65 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1965/ces65.csv",
                                   variable_name = "v335")
@@ -33,14 +33,99 @@ output_age <- sondr::match_and_update(main = output_age, ## vector to update
 
 
 ## ces68 -------------------------------------------------------------------
+#### 1. Get raw age variable vector
+raw_ces68 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1968/ces68.csv",
+                                  variable_name = "age")
+table(raw_ces68, useNA = "always")
+
+#### 2. clean variable
+clean_ces68 <- raw_ces68
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces68
+names(clean_ces68) <- sondr::generate_survey_ids(n_respondents = length(clean_ces68), ## number of respondents
+                                                 source_id = "ces68") ## source_id
+
+## 4. add clean to the master output
+output_age <- sondr::match_and_update(main = output_age, ## vector to update
+                                      updates = clean_ces68) ## vector with updates
 
 ## ces74 -------------------------------------------------------------------
+#### 1. Get raw age variable vector
+raw_ces74 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1974/ces74.csv",
+                                  variable_name = "v478")
+table(raw_ces74, useNA = "always")
 
+#### 2. clean variable
+clean_ces74 <- raw_ces74
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces68
+names(clean_ces74) <- sondr::generate_survey_ids(n_respondents = length(clean_ces74), ## number of respondents
+                                                 source_id = "ces74") ## source_id
+
+## 4. add clean to the master output
+output_age <- sondr::match_and_update(main = output_age, ## vector to update
+                                      updates = clean_ces74) ## vector with updates
 ## ces79 -------------------------------------------------------------------
+#### 1. Get raw age variable vector
+raw_ces79 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1979/ces79.csv",
+                                  variable_name = "v1535")
+table(raw_ces79, useNA = "always")
+
+#### 2. clean variable
+clean_ces79 <- raw_ces79
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces68
+names(clean_ces79) <- sondr::generate_survey_ids(n_respondents = length(clean_ces79), ## number of respondents
+                                                 source_id = "ces79") ## source_id
+
+## 4. add clean to the master output
+output_age <- sondr::match_and_update(main = output_age, ## vector to update
+                                      updates = clean_ces79) ## vector with updates
 
 ## ces84 -------------------------------------------------------------------
 
+#### 1. Get raw age variable vector
+raw_ces84 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1984/ces84.csv",
+                                  variable_name = "var437")
+table(raw_ces84, useNA = "always")
+
+#### 2. clean variable
+clean_ces84 <- raw_ces84
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces68
+names(clean_ces84) <- sondr::generate_survey_ids(n_respondents = length(clean_ces84), ## number of respondents
+                                                 source_id = "ces84") ## source_id
+
+## 4. add clean to the master output
+output_age <- sondr::match_and_update(main = output_age, ## vector to update
+                                      updates = clean_ces84) ## vector with updates
 ## ces88 -------------------------------------------------------------------
+
+#### 1. Get raw age variable vector
+raw_ces88 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1988/ces88.csv",
+                                  variable_name = "n1")
+table(raw_ces88, useNA = "always")
+
+#### 2. clean variable
+clean_ces88 <- NA
+clean_ces88_yrbirth <- raw_ces88
+table(clean_ces88_yrbirth, useNA = "always")
+clean_ces88_yrbirth[raw_ces88==9997 | raw_ces88==9998 | raw_ces88==9999] <- NA
+table(clean_ces88_yrbirth, useNA = "always")
+clean_ces88 <- 1988-clean_ces88_yrbirth
+table(clean_ces88, useNA = "always")
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces68
+names(clean_ces88) <- sondr::generate_survey_ids(n_respondents = length(clean_ces88), ## number of respondents
+                                                 source_id = "ces88") ## source_id
+
+## 4. add clean to the master output
+output_age <- sondr::match_and_update(main = output_age, ## vector to update
+                                      updates = clean_ces88) ## vector with updates
 
 ## ces93 -------------------------------------------------------------------
 
