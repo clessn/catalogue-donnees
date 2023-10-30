5363afa232daa81adb9b79c0bff9ac5043de0f60
hubcad25 <hubertcadieux@gmail.com>
2023-10-30 17:03:46 -0400

    add example to template

--- merging-souverainete/merging_files/ses_family_income.1.R
+++ merging-souverainete/merging_files/ses_family_income.1.R
@@ -2,7 +2,6 @@
 library(tidyverse)
 
 # Config ------------------------------------------------------------------
-### source config file to generate all the respondent_ids
 source("merging-souverainete/config.R")
 sample(ids, 20) ### 20 ids random
 ## empty vector where the clean values will go. same length as the n of ids.
@@ -10,33 +9,9 @@ output <- rep(NA, length(ids))
 ### each element in output is a respondent with a unique respondent id
 names(output) <- ids
 
-
 # Merging and cleaning ----------------------------------------------------
 
 ## ces65 -------------------------------------------------------------------
-
-### EXAMPLE WITH GENDER
-#### 1. Get raw gender variable vector
-raw_ces65 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1965/ces65.csv",
-                                  variable_name = "v337")
-table(raw_ces65, useNA = "always")
-
-#### 2. clean variable
-clean_ces65 <- NA
-clean_ces65[raw_ces65 == "female"] <- "female"
-clean_ces65[raw_ces65 == "male"] <- "male"
-table(clean_ces65)
-
-#### 3. name each element in clean (assign the respondent id to each person in the vector)
-##### source_id = ces65
-names(clean_ces65) <- sondr::generate_survey_ids(n_respondents = length(clean_ces65), ## number of respondents
-                                                 source_id = "ces65") ## source_id
-
-## 4. add clean to the master output
-output_gender <- sondr::match_and_update(main = output_gender, ## vector to update
-                                         updates = clean_ces65) ## vector with updates
-### END EXAMPLE WITH GENDER
-
 
 ## ces68 -------------------------------------------------------------------
 
