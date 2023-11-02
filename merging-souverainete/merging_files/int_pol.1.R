afc099b2e4161311e63e00eb1212c6b811840ce3
etienneprx <126438196+etienneprx@users.noreply.github.com>
2023-11-02 18:00:49 +0100

    int_pol jusqu'a ces 2000, yeah!

--- merging-souverainete/merging_files/int_pol.1.R
+++ merging-souverainete/merging_files/int_pol.1.R
@@ -44,11 +44,14 @@ output_intpol <- sondr::match_and_update(main = output_intpol, ## vector to upda
 ## ces74 -------------------------------------------------------------------
 
 raw_ces74 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1974/ces74.csv",
-                                  variable_name = "v36")
+                                  variable_name = "v12")
 table(raw_ces74, useNA = "always")
 
 #### 2. clean variable
-clean_ces74 <- raw_ces74
+clean_ces74 <- NA
+clean_ces74[raw_ces74=="very closely"] <- 1
+clean_ces74[raw_ces74=="fairly closely"] <- 0.5
+clean_ces74[raw_ces74=="not much at all"] <- 0
 
 table(clean_ces74, useNA = "always")
 
@@ -64,11 +67,78 @@ output_intpol <- sondr::match_and_update(main = output_intpol, ## vector to upda
 
 ## ces79 -------------------------------------------------------------------
 
+raw_ces79 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1979/ces79.csv",
+                                  variable_name = "v1022")
+table(raw_ces79, useNA = "always")
+
+#### 2. clean variable
+clean_ces79 <- NA
+clean_ces79[raw_ces79=="1"] <- 1
+clean_ces79[raw_ces79=="2"] <- 0.5
+clean_ces79[raw_ces79=="3"] <- 0
+
+table(clean_ces79, useNA = "always")
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces79
+names(clean_ces79) <- sondr::generate_survey_ids(n_respondents = length(clean_ces79), ## number of respondents
+                                                 source_id = "ces79") ## source_id
+
+## 4. add clean to the master output
+output_intpol <- sondr::match_and_update(main = output_intpol, ## vector to update
+                                         updates = clean_ces79) ## vector with updates
+
 ## ces84 -------------------------------------------------------------------
+
+raw_ces84 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1984/ces84.csv",
+                                  variable_name = "var017")
+table(raw_ces84, useNA = "always")
+
+#### 2. clean variable
+clean_ces84 <- NA
+clean_ces84[raw_ces84=="very closely"] <- 1
+clean_ces84[raw_ces84=="fairly closely"] <- 0.5
+clean_ces84[raw_ces84=="not much at all"] <- 0
+
+table(clean_ces84, useNA = "always")
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces84
+names(clean_ces84) <- sondr::generate_survey_ids(n_respondents = length(clean_ces84), ## number of respondents
+                                                 source_id = "ces84") ## source_id
+
+## 4. add clean to the master output
+output_intpol <- sondr::match_and_update(main = output_intpol, ## vector to update
+                                         updates = clean_ces84) ## vector with updates
+
 
 ## ces88 -------------------------------------------------------------------
 
+raw_ces88 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1988/ces88.csv",
+                                  variable_name = "a7")
+table(raw_ces88, useNA = "always")
+
+#### 2. clean variable
+clean_ces88 <- NA
+clean_ces88[raw_ces88=="very closely"] <- 1
+clean_ces88[raw_ces88=="fairly closely" | raw_ces88=="not very closely"] <- 0.5
+clean_ces88[raw_ces88=="not at all"] <- 0
+
+table(clean_ces88, useNA = "always")
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces88
+names(clean_ces88) <- sondr::generate_survey_ids(n_respondents = length(clean_ces88), ## number of respondents
+                                                 source_id = "ces88") ## source_id
+
+## 4. add clean to the master output
+output_intpol <- sondr::match_and_update(main = output_intpol, ## vector to update
+                                         updates = clean_ces88) ## vector with updates
+
+
 ## ces93 -------------------------------------------------------------------
+
+
 
 ## ces97 -------------------------------------------------------------------
 
