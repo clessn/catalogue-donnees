74c5e86edc47be7b43519ba3e807cd6dbf2d112b
etienneprx <126438196+etienneprx@users.noreply.github.com>
2023-11-30 22:02:40 +0100

    religiosité validé et enregistré

--- merging-souverainete/merging_files/ses_religiosity.1.R
+++ merging-souverainete/merging_files/ses_religiosity.1.R
@@ -61,7 +61,7 @@ names(clean_ces68) <- sondr::generate_survey_ids(n_respondents = length(clean_ce
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces68) ## vector with updates
-
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces68"))
 ## ces74 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces74 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1974/ces74.csv",
@@ -84,6 +84,7 @@ names(clean_ces74) <- sondr::generate_survey_ids(n_respondents = length(clean_ce
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces74) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces74"))
 ## ces79 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces79 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1979/ces79.csv",
@@ -105,6 +106,7 @@ names(clean_ces79) <- sondr::generate_survey_ids(n_respondents = length(clean_ce
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces79) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces79"))
 ## ces84 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces84 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1984/ces84.csv",
@@ -126,6 +128,7 @@ names(clean_ces84) <- sondr::generate_survey_ids(n_respondents = length(clean_ce
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces84) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces84"))
 ## ces88 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces88 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1988/ces88.csv",
@@ -148,6 +151,7 @@ names(clean_ces88) <- sondr::generate_survey_ids(n_respondents = length(clean_ce
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces88) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces88"))
 ## ces93 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces93 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1993/ces93.csv",
@@ -170,6 +174,7 @@ names(clean_ces93) <- sondr::generate_survey_ids(n_respondents = length(clean_ce
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces93) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces93"))
 ## ces97 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces97 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/1997/ces97.csv",
@@ -192,6 +197,7 @@ names(clean_ces97) <- sondr::generate_survey_ids(n_respondents = length(clean_ce
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces97) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces97"))
 ## ces2000 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces2000 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2000/ces2000.csv",
@@ -214,28 +220,30 @@ names(clean_ces2000) <- sondr::generate_survey_ids(n_respondents = length(clean_
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces2000) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2000"))
 ## ces2004 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces2004 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2004/ces2004.csv",
-                                  variable_name = "cps_s11")
+                                  variable_name = "ces04_cps_s11")
 table(raw_ces2004, useNA = "always")
 
 #### 2. clean variable
-clean_ces68 <- NA
-clean_ces68[raw_ces68 == 5] <- 0 # pas religieux
-clean_ces68[raw_ces68 == 4 | raw_ces68 == 3] <- 0.33 # un peu religieux/pas très religieux
-clean_ces68[raw_ces68 == 2] <- 0.66 # Assez religieux
-clean_ces68[raw_ces68 == 1] <- 1 # très religieux
-table(clean_ces68)
+clean_ces2004 <- NA
+clean_ces2004[raw_ces2004 == "not important at all"] <- 0 # pas religieux
+clean_ces2004[raw_ces2004 == "not very important"] <- 0.33 # un peu religieux/pas très religieux
+clean_ces2004[raw_ces2004 == "somewhat important"] <- 0.66 # Assez religieux
+clean_ces2004[raw_ces2004 == "very important"] <- 1 # très religieux
+table(clean_ces2004)
 
 #### 3. name each element in clean (assign the respondent id to each person in the vector)
 ##### source_id = ces65
-names(clean_ces68) <- sondr::generate_survey_ids(n_respondents = length(clean_ces68), ## number of respondents
-                                                 source_id = "ces68") ## source_id
+names(clean_ces2004) <- sondr::generate_survey_ids(n_respondents = length(clean_ces2004), ## number of respondents
+                                                 source_id = "ces2004") ## source_id
 
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
-                                              updates = clean_ces68) ## vector with updates
+                                              updates = clean_ces2004) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2004"))
 ## ces2006 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces2006 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2006/ces2006.csv",
@@ -258,6 +266,7 @@ names(clean_ces2006) <- sondr::generate_survey_ids(n_respondents = length(clean_
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces2006) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2006"))
 ## ces2008 -------------------------------------------------------------------
 #### 1. Get raw gender variable vector
 raw_ces2008 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2008/ces2008.csv",
@@ -280,6 +289,7 @@ names(clean_ces2008) <- sondr::generate_survey_ids(n_respondents = length(clean_
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces2008) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2008"))
 ## ces2011 -------------------------------------------------------------------
 raw_ces2011 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2011/ces2011.csv",
                                     variable_name = "CPS11_82")
@@ -301,6 +311,7 @@ names(clean_ces2011) <- sondr::generate_survey_ids(n_respondents = length(clean_
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces2011) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2011"))
 ## ces2015 -------------------------------------------------------------------
 raw_ces2015 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2015/ces2015.csv",
                                     variable_name = "relig_imp")
@@ -322,6 +333,7 @@ names(clean_ces2015) <- sondr::generate_survey_ids(n_respondents = length(clean_
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces2015) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2015"))
 ## ces2019 -------------------------------------------------------------------
 raw_ces2019 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2019/ces2019.csv",
                                     variable_name = "cps19_rel_imp")
@@ -343,6 +355,7 @@ names(clean_ces2019) <- sondr::generate_survey_ids(n_respondents = length(clean_
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces2019) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2019"))
 ## ces2021 -------------------------------------------------------------------
 raw_ces2021 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/ces/2021/ces2021.csv",
                                     variable_name = "cps21_rel_imp")
@@ -364,6 +377,7 @@ names(clean_ces2021) <- sondr::generate_survey_ids(n_respondents = length(clean_
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_ces2021) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "ces2021"))
 ## datagotchi_pilot1_2021 -------------------------------------------------------------------
 ## january -------------------------------------------------------------------
 ## february -------------------------------------------------------------------
@@ -377,17 +391,32 @@ output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vect
 
 #### 1. Get raw gender variable vector
 raw_datagotchi_pilot2_2022_fr <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/datagotchi_pilot2_2022/datagotchi_pilot2_2022.csv",
-                               variable_name = "FR_importantOfRelig")
+                               variable_name = "FR_importantOfRelig_1")
 table(raw_datagotchi_pilot2_2022_fr, useNA = "always")
 
 raw_en <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/datagotchi_pilot2_2022/datagotchi_pilot2_2022.csv",
-                               variable_name = "income_en")
+                               variable_name = "EN_importantRelig_1")
 table(raw_en, useNA = "always")
 
-raw <- coalesce(raw_fr, raw_en)
+raw_datagotchi_pilot2_2022 <- coalesce(raw_datagotchi_pilot2_2022_fr, raw_en)
 
 #### 2. clean variable
+clean_datagotchi_pilot2_2022 <- NA
+clean_datagotchi_pilot2_2022[(-1 < raw_datagotchi_pilot2_2022 & raw_datagotchi_pilot2_2022 < 26)] <- 0
+clean_datagotchi_pilot2_2022[(25 < raw_datagotchi_pilot2_2022 & raw_datagotchi_pilot2_2022 < 51)] <- 0.33
+clean_datagotchi_pilot2_2022[(50 < raw_datagotchi_pilot2_2022 & raw_datagotchi_pilot2_2022 < 76)] <- 0.66
+clean_datagotchi_pilot2_2022[(75 < raw_datagotchi_pilot2_2022 & raw_datagotchi_pilot2_2022 < 101)] <- 1
+table(clean_datagotchi_pilot2_2022, useNA = "always")
 
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces65
+names(clean_datagotchi_pilot2_2022) <- sondr::generate_survey_ids(n_respondents = length(clean_datagotchi_pilot2_2022), ## number of respondents
+                                                   source_id = "datagotchi_pilot2_2022") ## source_id
+
+## 4. add clean to the master output
+output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
+                                              updates = clean_datagotchi_pilot2_2022) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "datagotchi_pilot2_2022"))
 
 ## sondage_nationalisme_2022 -------------------------------------------------------------------
 raw_sondage_nationalisme_2022 <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/sondage_nationalisme_2022/sondage_nationalisme_2022.csv",
@@ -410,6 +439,7 @@ names(clean_sondage_nationalisme_2022) <- sondr::generate_survey_ids(n_responden
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_sondage_nationalisme_2022) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "sondage_nationalisme_2022"))
 ## quorum_mcq_pilote -------------------------------------------------------------------
 
 ### ICI IL FAUT MERGER FRANCAIS ET ANGLAIS
@@ -428,6 +458,22 @@ raw_quorum_mcq_pilote <- coalesce(raw_quorum_mcq_pilote_fr, raw_quorum_mcq_pilot
 table(raw_quorum_mcq_pilote, useNA = "always")
 
 #### 2. clean variable
+clean_quorum_mcq_pilote <- NA
+clean_quorum_mcq_pilote[raw_quorum_mcq_pilote == "Fortement en désaccord"] <- 0 # pas religieux
+clean_quorum_mcq_pilote[raw_quorum_mcq_pilote == "Plutôt en désaccord" | raw_quorum_mcq_pilote == "Neutre"] <- 0.33 # un peu religieux/pas très religieux
+clean_quorum_mcq_pilote[raw_quorum_mcq_pilote == "Plutôt d'accord"] <- 0.66 # Assez religieux
+clean_quorum_mcq_pilote[raw_quorum_mcq_pilote == "Fortement d'accord"] <- 1 # très religieux
+table(clean_quorum_mcq_pilote)
+
+#### 3. name each element in clean (assign the respondent id to each person in the vector)
+##### source_id = ces65
+names(clean_quorum_mcq_pilote) <- sondr::generate_survey_ids(n_respondents = length(clean_quorum_mcq_pilote), ## number of respondents
+                                                                     source_id = "quorum_mcq_pilote") ## source_id
+
+## 4. add clean to the master output
+output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
+                                              updates = clean_quorum_mcq_pilote) ## vector with updates
+table(sondr::extract_elements_with_prefix(output_religiosity, "quorum_mcq_pilote"))
 
 ## pes_elxn_2022_text -------------------------------------------------------------------
 raw_pes_elxn_2022_text <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/pes_elxn_2022/pes_elxn_2022_text.csv",
@@ -450,6 +496,8 @@ names(clean_pes_elxn_2022_text) <- sondr::generate_survey_ids(n_respondents = le
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_pes_elxn_2022_text) ## vector with updates
+
+table(sondr::extract_elements_with_prefix(output_religiosity, "pes_elxn_2022_text"))
 ## pco -------------------------------------------------------------------
 raw_pco <- sondr::load_variable(file = "_SharedFolder_catalogue-donnees/merging-souverainete/raw/pco/pco.csv",
                                     variable_name = "Q18.2.Pco2014")
@@ -471,10 +519,11 @@ names(clean_pco) <- sondr::generate_survey_ids(n_respondents = length(clean_pco)
 ## 4. add clean to the master output
 output_religiosity <- sondr::match_and_update(main = output_religiosity, ## vector to update
                                               updates = clean_pco) ## vector with updates
-
+table(sondr::extract_elements_with_prefix(output_religiosity, "pco"))
 # Output ------------------------------------------------------------------
 
 ### FACTORISE, LEVELS, etc.
-
+table(output_religiosity)
+hist(output_religiosity)
 ##### SAVE VECTOR WHERE??
-
+saveRDS(output_religiosity, "_SharedFolder_catalogue-donnees/merging-souverainete/clean/vectors/ses_religiosity.rds")
